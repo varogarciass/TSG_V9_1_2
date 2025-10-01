8702523ea3a1c3f6c4086f2dd4b1786fdd9fb4dd
varogarciass <varogarcias190@gmail.com>
2025-10-02 00:00:13 +0200

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,16 +1,8 @@
-// electron/preload.js
-// Puente entre el frontend (HTML/JS) y Electron (sistema)
+// Este archivo se ejecuta antes de que la página web cargue en la ventana de Electron.
+// Puedes exponer APIs seguras desde aquí si quieres, de momento lo dejamos básico.
 
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge } = require('electron');
 
-// Exponemos funciones seguras al frontend
-contextBridge.exposeInMainWorld("electronAPI", {
-  guardarIngreso: (data) => ipcRenderer.send("guardar-ingreso", data),
-  guardarGasto: (data) => ipcRenderer.send("guardar-gasto", data),
-  guardarSueldo: (data) => ipcRenderer.send("guardar-sueldo", data),
-  imprimirOrden: (data) => ipcRenderer.send("imprimir-orden", data),
-  generarFactura: (data) => ipcRenderer.send("generar-factura", data),
-  generarPresupuesto: (data) => ipcRenderer.send("generar-presupuesto", data),
-  hacerBackup: () => ipcRenderer.send("hacer-backup"),
-  cargarBackup: () => ipcRenderer.invoke("cargar-backup")
+contextBridge.exposeInMainWorld('api', {
+  version: () => 'Talleres Santa Gema v9.1'
 });
